1ad06b1af3cd4f19c27a0458716a519fc20b2a90
Kodomoppoi <jcmm.esft@gmail.com>
2025-01-05 11:52:41 -0300

    end of exercise, it was smaller than expected

--- Consective_Strings.py
+++ Consective_Strings.py
@@ -1,8 +1,12 @@
-def longest_consec(strarr):
-    for i in range(len(strarr)):
-        arr = 
-        print(strarr[i])
-
+def longest_consec(strarr, k):
+    o = 0
+    joined = []
+    strarr.sort(key=len, reverse=True)
+    if k <= 0 or k > len(strarr):
+        return ""
+    else:
+        joined.extend(strarr[:k])
+        joined = ''.join(joined)
+        return joined
         
         
-longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"])
