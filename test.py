a592c790f51970e28009ff5770033fc0ea3cc34c
Kodomoppoi <jcmm.esft@gmail.com>
2025-01-04 23:32:40 -0300

    initialized, not completed, got side tracked sadly, only here to simbolize my progress

---
+++ test.py
@@ -0,0 +1,8 @@
+def longest_consec(strarr):
+    for i in range(len(strarr)):
+        arr = 
+        print(strarr[i])
+
+        
+        
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"])
